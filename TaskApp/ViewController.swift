530b30ae70bcfc39fc2c76df89c299942094b523
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-07-07 02:01:52 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by WEBSYSTEM-MAC41 on 2024/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
